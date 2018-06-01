dfd0ba6f1439684e4e6e3ffdab29f71af7eb7bf5
Florian Weindl <theweindl@gmail.com>
2018-06-01 12:41:21 +0200

    Added slot define for Qt

--- Traktor.h
+++ Traktor.h
@@ -9,11 +9,14 @@ public:
 	Traktor();
 	~Traktor();
 
+	sf::CircleShape getShape();
+
+#ifdef QT_VERSION	//defines the slot only if there is a Qt Version defined, which mean you are in qt creator
+public slots:
+#endif
 	//Updates the current position of the tractor
 	void updatePosition(double const & la, double const & lo);
 	void updatePosition(GPSPoint const & position);
-
-	sf::CircleShape getShape();
 
 private:
 	GPSPoint mPosition;
